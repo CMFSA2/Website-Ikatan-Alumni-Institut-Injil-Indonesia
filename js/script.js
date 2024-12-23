27ed01cbe4aad3af3aef66555b3273ca459aff40
cmfsa2 <simarmata123firuli@gmail.com>
2024-12-23 18:42:18 +0700

    revisi swiper upcoming event 1

--- js/script.js
+++ js/script.js
@@ -12,36 +12,37 @@ document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+});
 
-  //Slider Upcoming Events
-  new Swiper(".up-card-wrapper", {
-    loop: true,
-    spaceBetween: 30,
+//Toggle Swiper Active
+new Swiper(".up-card-wrapper", {
+  loop: true,
+  spaceBetween: 30,
 
-    // Pagination Bullets
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+  // Pagination Bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    lockclass: "swiper-button-lock",
+  },
+
+  // Responsive Breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    768: {
+      slidesPerView: 2,
     },
-
-    // Responsive Breakpoints
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
+    1024: {
+      slidesPerView: 3,
     },
-  });
+  },
 });
