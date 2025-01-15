6e7ee632fdba85d67553bb18d869963313da1bd4
cmfsa2 <simarmata123firuli@gmail.com>
2025-01-15 16:22:22 +0700

    12.18 swiper belum selesai

--- Give Back/js/script.js
+++ Give Back/js/script.js
@@ -1,22 +1,5 @@
 // Initialize Swiper Instance
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "vertical",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
+const swiper = new Swiper(".slider-container", {
+  effect: "fade"
+  speed: 1300
 });
