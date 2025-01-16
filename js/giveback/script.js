8a5e092478cfc301c8820c7ce3367c0c7b462f07
cmfsa2 <simarmata123firuli@gmail.com>
2025-01-16 20:59:37 +0700

    revisi isi dan tambahan slider pokok doa Give Back

--- js/giveback/script.js
+++ js/giveback/script.js
@@ -1,24 +1,36 @@
 const sliderTabs = document.querySelectorAll(".slider-tab");
 const sliderIndicator = document.querySelector(".slider-indicator");
 
-const updatePagination = (tab, index) => {
-  sliderIndicator.style.transform = "translateX(${tab.offsetLeft - 20}px)";
-  sliderIndicator.style.width = "${tab.getBoundingClientRect().width}px";
+const updateIndicator = (tab, index) => {
+  sliderIndicator.style.transform = `translateX(${tab.offsetLeft - 20}px)`;
+  sliderIndicator.style.width = `${tab.getBoundingClientRect().width}px`;
 };
 
 // Initialize swiper instance
 const swiper = new Swiper(".slider-container", {
   effect: "fade",
   speed: 1300,
-  // autoplay: { delay: 4000 },
+  autoplay: { delay: 4000 },
+  on: {
+    //Update The Indicator On Slide Change
+    slideChange: () => {
+      const currentTabIndex = [...sliderTabs].indexOf(
+        sliderTabs[swiper.activeIndex]
+      );
+      updateIndicator(sliderTabs[swiper.activeIndex], currentTabIndex);
+    },
+  },
 });
 
-// Update The Slide and Pagination On Tab Click
+// Update The Slide and Indicator On Tab Click
 sliderTabs.forEach((tab, index) => {
   tab.addEventListener("click", () => {
     swiper.slideTo(index);
-    updatePagination(tab, index);
+    updateIndicator(tab, index);
   });
 });
 
-updatePagination(sliderTabs[0], 0);
+updateIndicator(sliderTabs[0], 0);
+window.addEventListener("resize", () =>
+  updateIndicator(sliderTabs[swiper.activeIndex], 0)
+);
