96e2e7a2712641e550f17b9227deaf01cc3d02e7
cmfsa2 <simarmata123firuli@gmail.com>
2025-01-16 14:52:40 +0700

    revisi major, penambahan slider bawah give back, 21.24

--- js/giveback/script.js
+++ js/giveback/script.js
@@ -1,5 +1,24 @@
+const sliderTabs = document.querySelectorAll(".slider-tab");
+const sliderIndicator = document.querySelector(".slider-indicator");
+
+const updatePagination = (tab, index) => {
+  sliderIndicator.style.transform = "translateX(${tab.offsetLeft - 20}px)";
+  sliderIndicator.style.width = "${tab.getBoundingClientRect().width}px";
+};
+
 // Initialize swiper instance
 const swiper = new Swiper(".slider-container", {
   effect: "fade",
   speed: 1300,
+  // autoplay: { delay: 4000 },
 });
+
+// Update The Slide and Pagination On Tab Click
+sliderTabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    swiper.slideTo(index);
+    updatePagination(tab, index);
+  });
+});
+
+updatePagination(sliderTabs[0], 0);
