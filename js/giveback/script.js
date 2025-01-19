ffafbbe1a12803f521fbd478a5676a32dec8b3fc
cmfsa2 <simarmata123firuli@gmail.com>
2025-01-20 01:44:13 +0700

    revisi major dan tambahan script kompatibilitas

--- js/giveback/script.js
+++ js/giveback/script.js
@@ -1,3 +1,19 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika menu di klik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Klik diluar sidebar untuk menghilangkan nav
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
 const sliderTabs = document.querySelectorAll(".slider-tab");
 const sliderIndicator = document.querySelector(".slider-indicator");
 
